7a6b73c0475652e9ab11fdcefb9c2277881cd80d
MalinduDissanayaka <dissanyaka_rplm_e22@engug.ruh.ac.lk>
2024-06-22 00:20:17 +0530

    created the home page related details

--- lib/profile_page.dart
+++ lib/profile_page.dart
@@ -1,5 +1,62 @@
+// import 'package:flutter/material.dart';
+// import 'package:google_sign_in/google_sign_in.dart';
+
+// class ProfilePage extends StatelessWidget {
+//   const ProfilePage({super.key});
+
+//   Future<void> logout() async {
+//     final GoogleSignIn googleSign = GoogleSignIn();
+//     await googleSign.signOut();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Column(
+//         mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//         crossAxisAlignment: CrossAxisAlignment.center,
+//         children: [
+//           Padding(
+//             padding: const EdgeInsets.only(top: 50.0),
+//             child: Center(
+//               child: const Text(
+//                 'Home page',
+//                 style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+//               ),
+//             ),
+//           ),
+//           Padding(
+//             padding: const EdgeInsets.only(bottom: 30.0),
+//             child: ElevatedButton(
+//               onPressed: () async {
+//                 await logout();
+//                 Navigator.pop(context);
+//               },
+//               style: ElevatedButton.styleFrom(
+//                 foregroundColor: Colors.white, backgroundColor: Colors.blue, // Set the text color to white
+//                 padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+//                 shape: RoundedRectangleBorder(
+//                   borderRadius: BorderRadius.circular(8),
+//                 ),
+//               ),
+//               child: const Text(
+//                 'Logout',
+//                 style: TextStyle(fontSize: 16),
+//               ),
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
+
+
+
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key});
@@ -9,20 +66,119 @@ class ProfilePage extends StatelessWidget {
     await googleSign.signOut();
   }
 
+  Future<List<dynamic>> fetchProducts() async {
+    final response = await http.get(Uri.parse('https://dummyjson.com/products'));
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      return data['products'];
+    } else {
+      throw Exception('Failed to load products');
+    }
+  }
+
+  void showProductDetails(BuildContext context, dynamic product) {
+    final List<String> images = product['images'].cast<String>();
+    if (images.isEmpty) {
+      images.add('https://via.placeholder.com/150'); // Dummy image if no images are available
+    }
+
+    showModalBottomSheet(
+      context: context,
+      builder: (BuildContext context) {
+        return Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                product['title'],
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 8),
+              Text(
+                'Price: \$${product['price']}',
+                style: TextStyle(fontSize: 20),
+              ),
+              SizedBox(height: 8),
+              Text(
+                'Rating: ${product['rating']}',
+                style: TextStyle(fontSize: 18),
+              ),
+              SizedBox(height: 8),
+              Text(product['description']),
+              SizedBox(height: 16),
+              Expanded(
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: images.length,
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Image.network(
+                        images[index],
+                        width: MediaQuery.of(context).size.width * 0.8,
+                        fit: BoxFit.cover,
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Page'),
+      ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Padding(
-            padding: const EdgeInsets.only(top: 50.0),
-            child: Center(
-              child: const Text(
-                'Home page',
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-              ),
+          Expanded(
+            child: FutureBuilder<List<dynamic>>(
+              future: fetchProducts(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Center(child: CircularProgressIndicator());
+                } else if (snapshot.hasError) {
+                  return Center(child: Text('Error: ${snapshot.error}'));
+                } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                  return Center(child: Text('No products found'));
+                } else {
+                  final products = snapshot.data!;
+                  return ListView.builder(
+                    itemCount: products.length,
+                    itemBuilder: (context, index) {
+                      final product = products[index];
+                      return Card(
+                        margin: EdgeInsets.all(10),
+                        child: ListTile(
+                          contentPadding: EdgeInsets.all(10),
+                          leading: Image.network(
+                            product['thumbnail'],
+                            width: 50,
+                            height: 50,
+                            fit: BoxFit.cover,
+                          ),
+                          title: Text(product['title']),
+                          subtitle: Text('Price: \$${product['price']}\nBrand: ${product['brand']}'),
+                          trailing: Icon(Icons.arrow_forward),
+                          onTap: () {
+                            showProductDetails(context, product);
+                          },
+                        ),
+                      );
+                    },
+                  );
+                }
+              },
             ),
           ),
           Padding(
@@ -33,7 +189,8 @@ class ProfilePage extends StatelessWidget {
                 Navigator.pop(context);
               },
               style: ElevatedButton.styleFrom(
-                foregroundColor: Colors.white, backgroundColor: Colors.blue, // Set the text color to white
+                foregroundColor: Colors.white,
+                backgroundColor: Colors.blue,
                 padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(8),
